4326209c3d4fd23aa6ae51a129de1b0f144f1eac
Blog Verse <blogverse25@gmail.com>
2025-12-17 18:14:24 +0700

    Fix image paths and correct typos in articleData.js

--- Braga Bandung/js/data/articleData.js
+++ Braga Bandung/js/data/articleData.js
@@ -6,7 +6,7 @@ const articleData = {
     date: "14 November 2025",
     author: {
         name: "Sammy",
-        avatar: "./../../Penulis/Sammy.jpg",
+        avatar: "./Penulis/Sammy.jpg",
         bio: "Perjalanan di Braga Bandung"
     },
     heroImage: "./images/Foto 1.jpg",
@@ -17,7 +17,7 @@ const articleData = {
             id: "pendahuluan",
             title: "Apa itu Braga?",
             content: `
-                <p>Braga adalah sebuah jalan yang berada di pusat Kota Bandung, Jawa barat, Indonesia. Sekarang Braga menjadi salah satu tempat wisata yang sering di kunjungi banayk orang. Di sana juga ada orang dari negara luar, bukan hanya Indonesia.</p>
+                <p>Braga adalah sebuah jalan yang berada di pusat Kota Bandung, Jawa barat, Indonesia. Sekarang Braga menjadi salah satu tempat wisata yang sering di kunjungi banyak orang. Di sana juga ada orang dari negara luar, bukan hanya Indonesia.</p>
             `
         },
         {
@@ -25,7 +25,7 @@ const articleData = {
             id: "arti",
             title: "Apa arti nama Braga?",
             content: `
-                <p>Nama Braga berasal dari bahasa Belanda yang berarti panggung atau pertunjukkan. Jalan ini diberi nama Braga karena dulunya dipenuhi dengan teater, kafe, dan tempat hiburan.Pada masa kolonial Belanda, jalan ini sangat terkenal sebagai pusan perbelanjaan dan gaya hidup. Jalan Braga menjadi tempat di mana para bangsawan Belanda dan para elit berjalan-jalan sehingga mendapatkan julukan <i><b>De Postweg</b></i> dan kemudian menjadi <i><b>Braga Weg</b></i>.</p>                
+                <p>Nama Braga berasal dari bahasa Belanda yang berarti panggung atau pertunjukkan. Jalan ini diberi nama Braga karena dulunya dipenuhi dengan teater, kafe, dan tempat hiburan. Pada masa kolonial Belanda, jalan ini sangat terkenal sebagai pusat perbelanjaan dan gaya hidup. Jalan Braga menjadi tempat di mana para bangsawan Belanda dan para elit berjalan-jalan sehingga mendapatkan julukan <i><b>De Postweg</b></i> dan kemudian menjadi <i><b>Braga Weg</b></i>.</p>                
             `
         },
         {
@@ -44,6 +44,7 @@ const relatedPosts = [
         id: 2,
         title: "Pabrik Mochi Arjuna Wijaya Sukabumi",
         date: "28 Mei 2025", 
-        image: "./../Pabrik Moci Sukabumi/images/Foto (5).jpg"
+        image: "./images/Foto (5).jpg",
+        href : "https://pabrik-mochi-sukabumi-blog-verse.vercel.app/"
     }
 ];
